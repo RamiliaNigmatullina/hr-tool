f9c964bb9ce3b15e4241f59ae9e2c778995d5157
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-11-09 13:32:31 +0300

    user profile added

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
       user = User.create(full_name: data["name"],
                          email: data["email"],
                          password: Devise.friendly_token[0, 20],
-                         profile_image_id: data["image"])
+                         profile_image: data["image"])
     end
     user
   end
