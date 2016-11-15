2e26ae1350fe4b7d15f7a44301827e6d51d5c5d3
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-11-15 19:47:29 +0300

    possibility to edit user role added (#27)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  expose_decorated :user
   expose_decorated :users, -> { User.sorted }
 
   def index
+  end
+
+  def update
+    user.update_attributes(user_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role, :level)
   end
 end
