3fb8b4021707d8399718899dcc593e253ab3d29a
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-11-19 22:02:34 +0300

    fix bug (#42)
    fix bug in feedback controller (#42)

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,7 +2,7 @@ class Invite < ActiveRecord::Base
   belongs_to :assessment
   belongs_to :user
 
-  has_one :feedback
+  has_one :feedback, dependent: :destroy
 
   validates :user, :assessment, presence: true
 
