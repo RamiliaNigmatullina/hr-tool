1546a6bbe9e7b94ad79fc553b255428b43ea0be0
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2016-12-12 19:21:29 +0300

    send emails

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,10 @@
 class ApplicationMailer < ActionMailer::Base
   layout "mailer"
+
+  def invite_created(user, assessment)
+    @assessment = assessment
+    @user = user
+
+    mail to: user.email
+  end
 end
