2b1300fa783d03cd8439e5e29df7707a4d1ef4c9
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2016-12-15 15:27:40 +0300

    fix for skill table

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20161212193157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "assessments", force: :cascade do |t|
     t.integer  "user_id"
     t.date     "date"
@@ -76,25 +70,25 @@ ActiveRecord::Schema.define(version: 20161212193157) do
   add_index "skills", ["department_id"], name: "index_skills_on_department_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "",         null: false
-    t.string   "encrypted_password",     limit: 255, default: "",         null: false
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",         null: false
+    t.string   "encrypted_password",     default: "",         null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string   "confirmation_token",     limit: 255
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
-    t.integer  "sign_in_count",                      default: 0
+    t.string   "unconfirmed_email"
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
-    t.datetime "created_at",                                              null: false
-    t.datetime "updated_at",                                              null: false
-    t.string   "full_name",              limit: 255
-    t.string   "role",                               default: "employee", null: false
-    t.integer  "level",                              default: 1,          null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "full_name"
+    t.string   "role",                   default: "employee", null: false
+    t.integer  "level",                  default: 1,          null: false
     t.string   "provider"
     t.string   "uid"
     t.string   "profile_image"
