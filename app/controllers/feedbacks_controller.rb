a45f88d5ecd92a95ac62edcd8612ffbe83fbbc56
Ramilia Nigmatullina <RamiliaNigmatullina@gmail.com>
2017-01-09 20:12:46 +0300

    code refactoring (#5)
    * fix alert message
    
    * code refactoring

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,11 +2,12 @@ class FeedbacksController < ApplicationController
   before_action :authorize!, only: %w(edit update destroy)
 
   expose :feedback
-  expose :assessment
-
   expose :feedbacks, -> { current_user.feedbacks.includes(assessment: :user) }
-  expose :skill_feedbacks, -> { feedback.skill_feedbacks.includes(:skill) }
+
   expose :skills, -> { fetch_skills }
+
+  expose_decorated :skill_feedbacks, -> { feedback.skill_feedbacks }
+  expose_decorated :assessment
 
   def new
     skills.each do |skill|
@@ -23,7 +24,7 @@ class FeedbacksController < ApplicationController
   end
 
   def update
-    feedback.update_attributes(feedback_params)
+    feedback.update(feedback_params)
     respond_with(feedback)
   end
 
